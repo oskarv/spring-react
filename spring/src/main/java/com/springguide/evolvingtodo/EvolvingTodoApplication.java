f083b74826bae23c43ae3415d27cd83631ad0fb1
illusi0n <miloshb92@hotmail.com>
2019-07-31 22:15:20 +0200

    Initial spring project setup

---
+++ spring/src/main/java/com/springguide/evolvingtodo/EvolvingTodoApplication.java
@@ -0,0 +1,13 @@
+package com.springguide.evolvingtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolvingTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvolvingTodoApplication.class, args);
+	}
+
+}
