1d019d2ef3c24a9d34dda5fd00ee23b5203099f9
illusi0n <miloshb92@hotmail.com>
2019-08-01 17:52:31 +0200

    Format using spaces instead of tabs

--- spring/src/test/java/com/springguide/evolvingtodo/EvolvingTodoApplicationTests.java
+++ spring/src/test/java/com/springguide/evolvingtodo/EvolvingTodoApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class EvolvingTodoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
