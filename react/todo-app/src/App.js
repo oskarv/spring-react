a3217f51d55b39bf54b9e5816b9d74e83fa58174
Oskar Vatic <oskarsmit@hotmail.com>
2019-08-04 20:32:56 +0200

    fix typo

--- react/todo-app/src/App.js
+++ react/todo-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-		<p>
+        <p>
           Spring + React: Guide to web development
         </p>
         <p>
